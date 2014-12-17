9a2f0517aa43efad9789d7895e5f2e61f06eab45
dorotheexploro <dorothy.wong51@gmail.com>
2014-12-17 10:46:35 -0800

    Update: styling and maps
    Fixed styling in sections. Added Find us and Contact section. Added
    Google maps.

--- header.php
+++ header.php
@@ -21,6 +21,19 @@
 	<script>try{Typekit.load();}catch(e){}</script>
 
   <script src="js/vendor/custom.modernizr.js"></script>
+	
+	<script src="http://maps.googleapis.com/maps/api/js"></script>
+	<script>
+	function initialize() {
+	  var mapProp = {
+	    center:new google.maps.LatLng(37.74264,-122.48275),
+	    zoom:15,
+	    mapTypeId:google.maps.MapTypeId.ROADMAP
+	  };
+	  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+	}
+	google.maps.event.addDomListener(window, 'load', initialize);
+	</script>
 
 
 </head>
--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php include("header.php"); ?>
 
 <header class="main nav">
-	<div class="nav title">
-		DR. CHRISTOPHER T. NICHOLS, DDS
-	</div>
+	<div class="nav title">CHRISTOPHER T. NICHOLS, D.D.S.</div>
 	<div class="nav links">
 		<span class="nav element"><img src="img/phone_icon.png" class="phone">(415) 661-3989</span>
 		<a href="#" class="nav element">CONTACT</a>
@@ -15,9 +13,9 @@
 
 <section class="hero">
 	<div class="hero text">
-		<h1 class="name">Dr. Christopher T. Nichols, DDS</h1>
-		<h1 class="dentistry">FAMILY AND COSMETIC DENTISTRY<h1>
-		<h3 class="since">Since 1988</h3>
+		<h1 id="name">Christopher T. Nichols, D.D.S.</h1>
+		<h1 id="dentistry">FAMILY AND COSMETIC DENTISTRY<h1>
+		<h3 id="since">Since 1988</h3>
 	</div>
 </section>
 
@@ -38,8 +36,10 @@
 
 <section class="cnichols">
 	<div class="statement">
-		<h2>Hi, I'm Dr. Christoper T. Nichols, DDS. I have built my practice around providing my customers with comfort and making the trip to dentist as easy and comfortable as possible.</h2>
-		<p>My background includes this degree from this educational institution. I have this experience</p>
+		<h1 id="meet">Meet</h1>
+		<h1 id="chris">Christopher T. Nichols</h1>
+		<h2 id="summary">Hi, I'm Dr. Christoper T. Nichols, DDS. I have built my practice around providing my customers comfort and making the trip to dentist as easy and comfortable as possible.</h2>
+		<p id="bio">My background includes this degree from this educational institution. I have this experience</p>
 	</div>
 </section>
 
@@ -48,13 +48,42 @@
 	<div class="lee profile image"></div>
 	<div class="veronica profile image"></div>
 	<div class="sandy profile image"></div>
-	<div class="joe profile image"></div>
 	<div class="hahn profile image"></div>
-	<div class="sandy profile image"></div>
+	<div class="seiko profile image"></div>
+	<div class="joe profile image"></div>
+</section>
+
+<section class="find">
+	<h1 class="find text">Find us on:</h1>
+	<a href="#" class="find element"><img class="yelp" src="###"/></a>
+	<a href="#" class="find element"><img class="other" src="###"/></a>
 </section>
 
 <section class="contact">
-	<>
+	<div class="hello">SAY HELLO!</div>
+	<div class="or">or</div>
+	<a href="#" class="appointment">REQUEST AN APPOINTMENT ONLINE</div>
+	<div class="contact info"><table border="0" style="max width=700px">
+		<tr>
+			<td>PHONE</td>
+			<td>415.661.3989</td>
+		</tr>
+		<tr>
+			<td>EMAIL</td>
+			<td>CNicholsDDS@ident.com</td>
+		<tr>
+			<td>ADDRESS</td>
+			<td>1539 Taraval St</td>
+		</tr>
+			<td></td>
+			<td>Suite 201</td>
+		</tr>
+		<tr>
+			<td></td>
+			<td>San Francisco, CA 94116</td>
+		</tr>
+		
+	<div id="googleMap" style="width:100%;height:400px;"></div>
 </section>
  
 <?php include("footer.php"); ?>
